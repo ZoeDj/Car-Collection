ded6cde5c6fc23908c91c78371a9c86991b42e4d
ZoeDj <studio3fun@gmail.com>
2019-09-22 00:22:05 -0400

    media queries added

--- cars/src/App.js
+++ cars/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Car from "./components/car.js";
 import Wrapper from "./components/wrapper.js";
 import cars from "./cars.json";
+
 import "./App.css";
 
 class App extends Component {
@@ -14,18 +15,25 @@ class App extends Component {
   };
   render() {
     return (
-      <Wrapper>
-        {this.state.cars.map(car => (
-          <Car
-            removeCar={this.removeCar}
-            id={car.id}
-            key={car.id}
-            brand={car.brand}
-            image={car.image}
-            color={car.color}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <h1>Car Collection</h1>
+        <h3>
+          You can remove the cars that are not your favorites by clicking on
+          "x".
+        </h3>
+        <Wrapper>
+          {this.state.cars.map(car => (
+            <Car
+              removeCar={this.removeCar}
+              id={car.id}
+              key={car.id}
+              brand={car.brand}
+              image={car.image}
+              color={car.color}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
